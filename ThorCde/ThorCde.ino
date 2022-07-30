7e9723f8291a0baa2b2b4eb5b53ad668730aaddd
amin mahjoub <t-ammahjoub@microsoft.com>
2022-07-29 17:10:14 -0700

    added a user interface for calibrating the INA

--- ThorCde/ThorCde.ino
+++ ThorCde/ThorCde.ino
@@ -20,7 +20,7 @@ int lastFire[] = {0,0,0,0};
 bool clear;
 int count[] = {0,0,0,0};
 float prevTime = -10000;
-
+float shuntValues[4];
 bool flag;
 bool buttonFlag[] = {false, false, false, false};
 bool button2pressFlag = false;
@@ -30,16 +30,240 @@ bool alertFlag[] {false, false, false, false};
 
 bool startFlag = false;
 
+/*
+	TODO:	
+		Calibration Choice Menu
+		Integrate calculate power and current functions with current implemention
+		Find the best way to create new files on SD card without having to delete 
+*/
+
+void clearDisplay() {
+	display.clearDisplay();
+	display.display();
+}
+void staticCalibrationChoice() {
+	display.setCursor(20, 0);
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+	display.println("Calibration Menu");
+
+	display.setCursor(0, 10);
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+	display.println("Yes");
+
+	display.setCursor(0, 20);
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+	display.println("No");
+
+}
+
+bool calibrationChoice() { 
+	while(1) {
+		Serial.println(buttonFlag[0]);
+		if (buttonFlag[0]) {
+			buttonFlag[0] = false;
+			if (count[0] == 1) {
+				count[0] = 0;
+			}
+			else {
+				count[0]++;
+			}
+		}
+		Serial.print("calibration choice count:");
+		Serial.println(count[0]);
+
+		staticCalibrationChoice();
+		
+		if (count[0] == 0) {
+			display.setTextColor(WHITE);
+			display.setCursor(50, 10);
+			display.println("<--");
+			display.display();
+
+			display.setTextColor(WHITE, BLACK);
+			display.setCursor(50, 20);
+			display.println("   ");
+			display.display();
+			if (buttonFlag[1]) {
+				buttonFlag[1] = false;
+				return true;
+				
+			}
+
+		}
+		else if (count[0] == 1) {
+			display.setTextColor(WHITE, BLACK);
+			display.setCursor(50, 10);
+			display.println("   ");
+			display.display();
+
+			display.setTextColor(WHITE);
+			display.setCursor(50, 20);
+			display.println("<--");
+			display.display();
+
+			if (buttonFlag[1]) {
+				buttonFlag[1] = false;
+				return false;
+				
+			}
+		}
+	}
+	
+}
+void staticCalibrationMenu() {
+	display.setTextSize(.01);
+	display.setTextColor(WHITE);
+
+	display.setCursor(0, 0);
+	display.println("Load 50 ohm");
+
+	display.setTextSize(.01);
+	display.setCursor(0, 10);
+	display.println("Load 100 ohm");
+
+
+	display.setTextSize(.01);
+	display.setCursor(0, 20);
+	display.println("Load 200 Ohm");
+
+	display.setCursor(100, 20);
+	display.println("INA");
+
+	display.display();
+}
+
+void showCalRes(float shuntRes) {
+	display.clearDisplay();
+	display.display();
+	display.setCursor(0, 0);
+	display.setTextColor(WHITE);
+	display.println("Calibrated Shunt Res: ");
+	display.setCursor(60, 20);
+	display.println(String(shuntRes));
+	display.display();
+	delay(2000);
+	display.clearDisplay();
+	display.display();		
+}
+void calibrationLoadMenu() {
+	clearDisplay();
+	display.setCursor(0, 10);
+	display.setTextColor(WHITE);
+	display.println("Calibrating...");
+	display.display();
+
+}
+int calNum = 0;
+int prevCalNum = 100;
+
+void calibrationMenu() {
+	if (buttonFlag[0]) {
+		count[0]++;
+		if (count[0] == 3) {
+			count[0] = 0;
+		}
+		buttonFlag[0] = false;
+	}
+
+	staticCalibrationMenu();
+	//Title
+	
+	if (calNum != prevCalNum) {
+		display.setCursor(118, 20);
+		display.setTextColor(WHITE, BLACK);
+		display.println(" ");
+		display.display();
+		display.setCursor(118, 20);
+		display.setTextColor(WHITE);
+		display.println(String(calNum));
+		prevCalNum = calNum;
+	}
+
+	if (count[0] == 0) {
+		display.setCursor(75, 0);
+		display.println("<--");
+		display.display();
+		display.setTextColor(WHITE, BLACK);
+		display.setCursor(75, 10);
+		display.println("   ");
+		display.setCursor(75, 20);
+		display.println("   ");
+		display.display();
+		if (buttonFlag[1]) {
+			calibrationLoadMenu();
+			float res = calibrateINA(30,calNum,50,shuntValues[calNum]);
+			prevCalNum = calNum;
+			calNum++;
+			showCalRes(res);
+			buttonFlag[1] = false;
+			clearDisplay();
+		}
+	}
+
+	if (count[0] == 1) {
+		display.setCursor(75, 10);
+		display.println("<--");
+		display.display();
+		display.setTextColor(WHITE, BLACK);
+		display.setCursor(75, 0);
+		display.println("   ");
+		display.setCursor(75, 20);
+		display.println("   ");
+		display.display();
+
+		if (buttonFlag[1]) {
+			calibrationLoadMenu() ;
+			float res = calibrateINA(30,calNum,100,shuntValues[calNum]);
+			
+			prevCalNum = calNum;
+			calNum++;
+			showCalRes(res);
+			buttonFlag[1] = false;
+			clearDisplay();
+		}
+
+	}
+	if (count[0] == 2) {
+		display.setCursor(75, 20);
+		display.println("<--");
+		display.display();
+		display.setTextColor(WHITE, BLACK);
+		display.setCursor(75, 0);
+		display.println("   ");
+		display.setCursor(75, 10);
+		display.println("   ");
+		display.display();
+
+		if (buttonFlag[1]) {
+			calibrationLoadMenu() ;
+			float res = calibrateINA(30,calNum,200,shuntValues[calNum]);
+			prevCalNum = calNum;
+			calNum++;
+			showCalRes(res);
+			buttonFlag[1] = false;
+			clearDisplay();
+		}
+	}
+	
+	
+}
 //Display Functions
 void staticStartMenu() {
 	display.setCursor(30, 0);
 	display.setTextSize(1);
 	display.setTextColor(WHITE);
 	display.println("POWER LOGGER");
-	display.setCursor(20, 20);
+	display.setCursor(25, 15);
 	display.setTextSize(1);
 	display.setTextColor(WHITE);
-	display.println("press Button4 to Start");
+	display.println("Press Button 4");
+	display.setCursor(45, 25);
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+	display.println("to Start");
 	display.display();
 }
 void staticMenu() {
@@ -71,19 +295,7 @@ void staticMenu() {
 	Power: 			mW
 */
 
-int prevCount = 1000;
-void DisplayData(float arr[]) {
-	if (buttonFlag[0] == true) {
-		count[0]++;
-		if (count[0] == 4) {
-			count[0] = 0;
-    	}
-    	
-    	buttonFlag[0] = false;
-		Serial.print("count:");
-		Serial.println(count[0]);
-		
-	}
+void staticDisplayDataMenu() {
 	Serial.println("here");
 	display.setTextSize(.0001);
 	display.setTextColor(WHITE);
@@ -109,6 +321,23 @@ void DisplayData(float arr[]) {
 	display.println("mW");
 	display.display();
 	delay(100);
+}
+
+int prevCount = 1000;
+void DisplayData(float arr[]) {
+	if (buttonFlag[0] == true) {
+		count[0]++;
+		if (count[0] == 4) {
+			count[0] = 0;
+    	}
+    	
+    	buttonFlag[0] = false;
+		Serial.print("count:");
+		Serial.println(count[0]);
+		
+	}
+	
+	staticDisplayDataMenu();
 
 	if (prevCount != count[0]) {
 		display.setCursor(120, 0);
@@ -120,6 +349,7 @@ void DisplayData(float arr[]) {
 		display.println(titleString);
 		prevCount = count[0];
 	}
+
 	display.setTextSize(.0001);
 	display.setTextColor(WHITE);
 	//clear current values from display
@@ -175,8 +405,6 @@ void MoveCursor() {
   }
   
   if (count[0] == 0) {
-    // if callbutton1 pressed here
-    // call set =
     display.setCursor(70, 0);
     display.println("<--");
     display.setTextColor(WHITE, BLACK);
@@ -185,7 +413,9 @@ void MoveCursor() {
     display.setCursor(70, 20);
     display.println("   ");
     display.display();
+	
     if (buttonFlag[1]) {
+		shuntValues[inaNum] = 100;
       if(inaNum == 0) {
         Serial.print("INA0 100 set");
         inaArray[inaNum].setCalibration(100);
@@ -217,7 +447,9 @@ void MoveCursor() {
 		display.setCursor(70, 20);
 		display.println("   ");
 		display.display();
+
 		if (buttonFlag[1]) {
+			shuntValues[inaNum] = 1;
 			if(inaNum == 0) {
 				Serial.print("INA0 1 set");
 				inaArray[inaNum].setCalibration(1);
@@ -237,55 +469,66 @@ void MoveCursor() {
 			buttonFlag[1] = false;
 			inaNum++;
 		}
-  }
+	}
 
-  if (count[0] == 2) {
-    display.setCursor(70, 20);
-    display.println("<--");
-    display.setTextColor(WHITE, BLACK);
-    display.setCursor(70, 0);
-    display.println("   ");
-    display.setCursor(70, 10);
-    display.println("   ");
-    display.display();
-     if (buttonFlag[1]) {
-      if(inaNum == 0) {
-        Serial.print("INA0 0.01 set");
-        inaArray[inaNum].setCalibration(0.01);
-      }
-      else if(inaNum == 1) {
-        Serial.print("INA1 0.01 set");
-        inaArray[inaNum].setCalibration(.01);
-      }
-      else if (inaNum == 2) {
-        Serial.print("INA2 0.01 set");
-       inaArray[inaNum].setCalibration(.01);
-      }
-      else if(inaNum == 3) {
-        Serial.print("INA3 0.01 set");
-        inaArray[inaNum].setCalibration(.01);
-      }
-      count[1] = 0;
-	  buttonFlag[1] = false;
-      inaNum++;
-    } 
-  }
+	if (count[0] == 2) {
+		display.setCursor(70, 20);
+		display.println("<--");
+		display.setTextColor(WHITE, BLACK);
+		display.setCursor(70, 0);
+		display.println("   ");
+		display.setCursor(70, 10);
+		display.println("   ");
+		display.display();
+
+		if (buttonFlag[1]) {
+			shuntValues[inaNum] = 0.01;
+		if(inaNum == 0) {
+			Serial.print("INA0 0.01 set");
+			inaArray[inaNum].setCalibration(0.01);
+		}
+		else if(inaNum == 1) {
+			Serial.print("INA1 0.01 set");
+			inaArray[inaNum].setCalibration(.01);
+		}
+		else if (inaNum == 2) {
+			Serial.print("INA2 0.01 set");
+			inaArray[inaNum].setCalibration(.01);
+		}
+		else if(inaNum == 3) {
+			Serial.print("INA3 0.01 set");
+			inaArray[inaNum].setCalibration(.01);
+		}
+
+		count[1] = 0;
+		buttonFlag[1] = false;
+		inaNum++;
+	} 
+}
 
   
 }
 
-void calibrateINA(int iterations) {
-  float total = 0;
-  for (int i = 0; i< iterations; i++) {
-    float voltage = inaArray[0].readVoltage();
-    total = total + inaArray[0].calculateShuntResitance(100, 5,voltage,1);
-   
-  }
-  total = total/iterations;
-  inaArray[0].setShuntRes(total);
-  Serial.print("Shunt Res: ");
-  Serial.println(total);
-  delay(10000);
+float calibrateINA(int iterations, int inaIndex, float load, float shuntRes) {
+	float total = 0;
+	float inputVoltage;
+	if (shuntRes == 1 || shuntRes == 0.01) {
+		inputVoltage = 5;
+	}
+	else if (shuntRes == 100) {
+		inputVoltage = 0.04;
+	}
+
+	for (int i = 0; i< iterations; i++) {
+		float voltage = inaArray[inaIndex].readVoltage();
+		total = total + inaArray[inaIndex].calculateShuntResitance(100, inputVoltage ,voltage,shuntRes);
+	}
+
+	total = total/iterations;
+	inaArray[inaIndex].setShuntRes(total);
+	Serial.print("Shunt Res: ");
+	Serial.println(total);
+	return total;
 }
 
 
@@ -315,7 +558,6 @@ void setup() {
   // attachInterrupt(digitalPinToInterrupt(ALERT2_PIN), alertTwoInterupt, FALLING);
   // attachInterrupt(digitalPinToInterrupt(ALERT3_PIN), alertThreeInterupt, FALLING);
   
-
   inaArray[0].reset();
   for (int i=0; i<4; i++) {
     inaArray[i].reset();
@@ -325,6 +567,8 @@ void setup() {
 }
 
 bool first = true;
+bool tesy = true;
+bool cal;
 void loop() {
 	if (startFlag) {
 		if(first) {
@@ -332,14 +576,6 @@ void loop() {
 			display.display();
 			first = false;
 		}
-		
-		
-	//  for (int i =0; i < 4; i++) {
-	//     if (alertFlag[i] = true) {
-	//       Serial.print("alert");
-	//       alertFlag[i] = false;
-	//     }
-	//   }
 		if(inaNum < 4) {
 			staticMenu();
 			MoveCursor();
@@ -356,52 +592,71 @@ void loop() {
 					count[i] = 0;
 				}
 			}
-
-			// check for any interupts 
-			DateTime now = rtc.now();
-
-			for (int i = 0; i < 4; i++) {
-				inputData[i] = inaArray[i].readVoltage();
-				inputData[i+4] = inaArray[i].readCurrent();
-				inputData[i+8] = inaArray[i].readPower();
-			}
-
-			inputData[12] = now.second();
-			String testString;
-			File dataFile = SD.open(filename, FILE_WRITE);
-			String time = String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second());
-
-			for (int i =0; i < 13; i++) {
-				if (i < 12) {
-					testString.concat(inputData[i]*pow(10,6));
-					testString.concat(",");
-				}
-				else {
-					testString.concat(time);
-					// testString.concat(" | ");
-				}
-			}
-
-			// if (dataFile) {
-			
-			//   Serial.println(testString);
-			//   dataFile.println(testString);
-			//   dataFile.close();
-			//   delay(10);
-			// }
-		
-			if (inputData[12] != prevTime) {
-				DisplayData(inputData);
-				prevTime = inputData[12];
+			if (tesy) {
+				cal = calibrationChoice();
+				tesy = false;
+				display.clearDisplay();
+				display.display();
+				count[0] = 0;
 			}
 			
+			if (cal && calNum < 4) {
+				calibrationMenu();
+				Serial.println(count[0]);
+				delay(1000);
+				if(calNum == 3) {
+					count[0] = 0;
+ 				}
+				
+			}
+			else {
+				// check for any interupts 
+				DateTime now = rtc.now();
+
+				for (int i = 0; i < 4; i++) {
+					inputData[i] = inaArray[i].readVoltage();
+					inputData[i+4] = inaArray[i].calculateCurrent(inputData[i]);
+					inputData[i+8] = inaArray[i].calculatePower(inputData[i+4]);
+				}
+
+				inputData[12] = now.second();
+				String testString;
+				File dataFile = SD.open(filename, FILE_WRITE);
+				String time = String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second());
+
+				for (int i =0; i < 13; i++) {
+					if (i < 12) {
+						testString.concat(inputData[i]*pow(10,6));
+						testString.concat(",");
+					}
+					else {
+						testString.concat(time);
+						// testString.concat(" | ");
+					}
+				}
+
+				// if (dataFile) {
+			
+				//   Serial.println(testString);
+				//   dataFile.println(testString);
+				//   dataFile.close();
+				//   delay(10);
+				// }
+			
+				if (inputData[12] != prevTime) {
+					DisplayData(inputData);
+					prevTime = inputData[12];
+				}
+			}
 		}
-
 	}
 	else {
 		if (!first) {
 			display.clearDisplay();
 			display.display();
+			for (int i=0; i<4; i++) {
+    			inaArray[i].reset();
+  			}
 		}
 		//start menu 
 		staticStartMenu();
@@ -410,6 +665,9 @@ void loop() {
 		for (int i = 0; i < 4; i++) {
 			count[i] = 0;
 		}
+		prevCount = 1000;
+		previnaNum = -1000;
+		tesy = true;
 	}
 	
 
